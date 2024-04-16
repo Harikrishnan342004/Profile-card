c32b5c3bce7dc287832ce2295b944ef457ef11c8
Hari <harikrishnan53828@gmail.com>
2024-04-17 02:34:04 +0530

    seconf

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import BackgroundAnimation from './BackgroundAnimation';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <BackgroundAnimation />
+      <img src="./image.png" alt="" className='middle-image' />
+      
+      
     </div>
   );
 }
---
+++ myapp/src/BackgroundAnimation.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import './BackgroundAnimation.css';
+
+const BackgroundAnimation = () => {
+  return (
+    <div className="background-animation">
+      <div className='container'>
+        <div className="bubble">
+            <span style={{'--i': 11}}> <i class="devicon-electron-original"></i> </span>
+            <span style={{'--i': 12}}>  
+            <i class="devicon-html5-plain"></i>
+           </span>
+            <span style={{'--i': 24}}>
+            <i class="devicon-codepen-original"></i>
+          </span>
+            <span style={{'--i': 10}}>
+            <i class="devicon-githubcodespaces-plain"></i>
+          </span>
+            <span style={{'--i': 23}}>
+            <i class="devicon-vscode-plain"></i>
+          </span>
+            <span style={{'--i': 18}}>
+            <i class="devicon-python-plain"></i>
+          </span>
+            <span style={{'--i': 16}}>
+            <i class="devicon-jupyter-plain"></i>
+          </span>
+            <span style={{'--i': 19}}>
+            <i class="devicon-javascript-plain"></i>
+          </span>
+            <span style={{'--i': 20}}>
+            <i class="devicon-json-plain"></i>
+          </span>
+            <span style={{'--i': 22}}>
+            <i class="devicon-css3-plain"></i>
+          </span>
+            <span style={{'--i': 25}}>
+            <i class="devicon-tailwindcss-original"></i>
+          </span>
+            <span style={{'--i': 18}}>
+            <i class="devicon-bootstrap-plain"></i>
+          </span>
+            <span style={{'--i': 21}}></span>
+            <span style={{'--i': 15}}>
+            <i class="devicon-reactbootstrap-original"></i>
+          </span>
+            <span style={{'--i': 13}}> 
+            <i class="devicon-babel-plain"></i>
+           </span>
+            <span style={{'--i': 26}}>
+            <i class="devicon-canva-original"></i>
+          </span>
+            <span style={{'--i': 17}}>
+            <i class="devicon-jupyter-plain"></i>
+          </span>
+            <span style={{'--i': 13}}>
+            <i class="devicon-pycharm-plain"></i>
+          </span>
+            <span style={{'--i': 28}}>
+            <i class="devicon-react-original"></i>
+          </span>
+
+
+            <span style={{'--i': 11}}>
+              
+            <i class="devicon-electron-original"></i>
+           
+            </span>
+            <span style={{'--i': 12}}>
+            <i class="devicon-nodejs-plain"></i>
+          </span>
+            <span style={{'--i': 24}}>
+            <i class="devicon-php-plain"></i>
+          </span>
+            <span style={{'--i': 10}}>
+            <i class="devicon-notion-plain"></i>
+          </span>
+            <span style={{'--i': 23}}>
+            <i class="devicon-git-plain"></i>
+          </span>
+            <span style={{'--i': 18}}>
+            <i class="devicon-github-original"></i>
+          </span>
+            <span style={{'--i': 16}}>
+            <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+          </span>
+            <span style={{'--i': 19}}>
+            <i class="devicon-android-plain"></i>
+          </span>
+            <span style={{'--i': 20}}>
+            <i class="devicon-amazonwebservices-plain-wordmark"></i>
+          </span>
+            <span style={{'--i': 22}}>
+            <i class="devicon-netlify-plain"></i>
+          </span>
+            <span style={{'--i': 25}}>
+            <i class="devicon-chrome-plain"></i>
+          </span>
+            <span style={{'--i': 18}}>
+            <i class="devicon-bulma-plain"></i>
+          </span>
+            <span style={{'--i': 21}}>
+            <i class="devicon-firefox-plain"></i>
+          </span>
+            <span style={{'--i': 15}}>
+            <i class="devicon-c-plain"></i>
+          </span>
+            <span style={{'--i': 13}}>
+            <i class="devicon-azuresqldatabase-plain"></i>
+          </span>
+            <span style={{'--i': 26}}>
+            <i class="devicon-mongodb-plain"></i>
+          </span>
+            <span style={{'--i': 17}}>
+            <i class="devicon-visualstudio-plain"></i>
+          </span>
+            <span style={{'--i': 13}}>
+            <i class="devicon-mysql-original"></i>
+          </span>
+            <span style={{'--i': 28}}><i class="devicon-chrome-plain"></i></span>
+            <span style={{'--i': 26}}>
+            <i class="devicon-canva-original"></i>
+          </span>
+          <span style={{'--i': 24}}>
+            <i class="devicon-php-plain"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BackgroundAnimation;
