77521510d90ac3a2621cd383a172882c5ba74cd3
Hari <harikrishnan53828@gmail.com>
2024-04-18 13:37:08 +0530

    third

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,15 @@
-
 import './App.css';
 import BackgroundAnimation from './BackgroundAnimation';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <BackgroundAnimation />
       <img src="./image.png" alt="" className='middle-image' />
+      <Footer></Footer>
+      
+
       
       
     </div>
