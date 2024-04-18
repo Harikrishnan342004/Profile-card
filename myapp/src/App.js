4e2e46a51d972ed509c5268b3f45cc6804e02fc6
Hari <harikrishnan53828@gmail.com>
2024-04-19 02:01:26 +0530

    hari

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,17 +1,41 @@
+// import './App.css';
+// import BackgroundAnimation from './BackgroundAnimation';
+// import Footer from './Footer';
+// import DarkMode from './DarkMode/DarkMode';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <DarkMode></DarkMode>
+//       <BackgroundAnimation />
+      
+//       <img src="./profile.png" alt="" className='middle-image' />
+//       <Footer></Footer>
+      
+
+      
+      
+//     </div>
+//   );
+// }
+
+// export default App;
 import './App.css';
 import BackgroundAnimation from './BackgroundAnimation';
 import Footer from './Footer';
+import DarkMode from './DarkMode/DarkMode';
 
 function App() {
   return (
-    <div className="App">
+    <div className={`App ${localStorage.getItem("selectedTheme")}`}>
+      <DarkMode />
       <BackgroundAnimation />
-      <img src="./image.png" alt="" className='middle-image' />
-      <Footer></Footer>
-      
-
-      
-      
+      <img 
+        src={localStorage.getItem("selectedTheme") === "dark" ? "./image.png" : "./profile.png"} 
+        alt="" 
+        className='middle-image' 
+      />
+      <Footer />
     </div>
   );
 }
